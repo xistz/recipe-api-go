bca2dcae4366a64c6a00ed06b464f432324b17eb
Jonathan Chan <tmxistz@gmail.com>
2020-09-30 22:18:32 +0900

    added httprouter

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -14,10 +16,20 @@ func main() {
 	if !ok {
 		dsn = "retailai:zH4tAwEfMUL7x3nM@(db:3306)/recipes"
 	}
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+	addr := ":" + port
 
 	db, err := sqlx.Connect("mysql", dsn)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer db.Close()
+
+	r := httprouter.New()
+
+	log.Println("Listening on port", port)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
