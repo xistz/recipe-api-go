179e504536709e549d79109c20886e1e0b62bb9d
Jonathan Chan <tmxistz@gmail.com>
2020-10-02 15:57:15 +0900

    defined Recipe model

---
+++ model.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+// Recipe defines model for recipe
+type Recipe struct {
+	ID              int64     `json:"id"`
+	Title           string    `json:"title"`
+	PreparationTime string    `json:"preparation_time"`
+	Serves          string    `json:"serves"`
+	Ingredients     string    `json:"ingredients"`
+	Cost            int       `json:"cost"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
